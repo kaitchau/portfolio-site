d121bfc49c2d9455abb41fb7c90afda3403a5e49
Kaitlyn Chau <kaitlyn.chau@stonybrook.edu>
2022-06-09 17:19:34 -0400

    template changes

--- app/__init__.py
+++ app/__init__.py
@@ -6,43 +6,30 @@ load_dotenv()
 app = Flask(__name__)
 
 
+isKayla = True
+
 @app.route('/')
-def index():
-    return render_template('index.html', title="Gorilla Gang Portfolio", url=os.getenv("URL"))
+def kaitlyn_index():
+    return render_template('kaitlyn_index.html', url=os.getenv("URL"))
 
-@app.route('/xuo')
-def xuo():
-    return render_template('xuo.html', title="Set Lynn Portfolio")
+@app.route('/kaitlyn_about')
+def kaitlyn_about():
+    return render_template('kaitlyn_about.html', url=os.getenv("URL"))
 
-@app.route('/xuo/hobbies')
-def xuo_jinja():
-    return render_template('xuo_jinja.html', title="Set Hobbies")
+@app.route('/kaitlyn_experience')
+def kaitlyn_experience():
+    return render_template('kaitlyn_experience.html', url=os.getenv("URL"))
 
-@app.route('/chuu')
-def chuu():
-    return render_template('chuu.html', title="Kaitlyn Chau Portfolio")
+@app.route('/kaitlyn_education')
+def kaitlyn_education():
+    return render_template('kaitlyn_education.html', url=os.getenv("URL"))
 
-@app.route('/chuu/hobbies')
-def chuu_hobbies():
-    return render_template('chuu_hobbies.html', title="Kaitlyn Chau's Hobbies")
+@app.route('/kaitlyn_hobbies')
+def kaitlyn_hobbies():
+    return render_template('kaitlyn_hobbies.html', url=os.getenv("URL"))
 
-@app.route('/chuu/travel')
-def chuu_travel():
-    return render_template('chuu_travel.html', title="Kaitlyn Chau's Travel'")
-
-@app.route('/chuu/experience')
-def chuu_experience():
-    return render_template('chuu_experience.html', title="Kaytlyn's Experience")
-
-@app.route('/chuu/education')
-def chuu_education():
-    return render_template('chuu_education.html', title="Kaitlyn's Education")
-
-@app.route('/diaz')
-def diaz():
-    return render_template('diaz.html', title="Daniel Diaz Portfolio")
-
-
-
+@app.route('/kaitlyn_places')
+def kaitlyn_places():
+    return render_template('kaitlyn_places.html', url=os.getenv("URL"))
 
 
