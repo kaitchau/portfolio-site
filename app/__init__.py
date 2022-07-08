ec9683fdf75af57bee00667c56c17469bd7cbb83
Kaitlyn Chau <kaitlyn.chau@stonybrook.edu>
2022-07-08 12:41:33 -0400

    updated init file and added Dockerfile

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,23 @@ from playhouse.shortcuts import model_to_dict
 load_dotenv()
 app = Flask(__name__)
 
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+    user=os.getenv("MYSQL_USER"),
+    password=os.getenv("MYSQL_PASSWORD"),
+    host=os.getenv("MYSQL_HOST"),
+    port=3306
+    )
+
+# mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+# user=os.getenv("MYSQL_USER"),
+# password=os.getenv("MYSQL_PASSWORD"),
+# host=os.getenv("MYSQL_HOST"),
+# port=3306
+# )
 
 isKayla = True
 
@@ -36,13 +53,6 @@ def kaitlyn_places():
     return render_template('kaitlyn_places.html', url=os.getenv("URL"))
 
 # app = Flask(__name__)
-
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-user=os.getenv("MYSQL_USER"),
-password=os.getenv("MYSQL_PASSWORD"),
-host=os.getenv("MYSQL_HOST"),
-port=3306
-)
 
 print(mydb)
 
