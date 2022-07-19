ebc37127f41b403038c8cfa77d7fc1164839d2ee
Kaitlyn Chau <kaitlyn.chau@stonybrook.edu>
2022-07-19 18:30:09 -0400

    updated redeploy-site.sh script, removed mysql-server and now using docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,24 +6,9 @@ cd /root/portfolio-site
 #make sure to get latest github repo version
 git fetch && git reset origin/main --hard
 
-#kill the old virtual env
-rm -rf python3-virtualenv
+#first spin containers down to prevent out of memory issues on
+#our VPS instances when building in the next step
+docker compose -f docker-compose.prod.yml down
 
-#makes the virtual env
-python3 -m venv python3-virtualenv
-#activate the virtual env
-source python3-virtualenv/bin/activate
-
-#make sure pip is latest update
-pip install --upgrade pip
-#make sure to install pip's requirements
-pip install -r requirements.txt
-
-#restarts the service from /etc/systemd/system/myportfolio.service
-systemctl restart myportfolio
-
-#note: if make changes to the myportfolio.service file, then need to run the following cmd
-# systemctl daemon-reload
-
-#note: run the following cmd to check the status of the systemd service
-# systemctl status myportfolio
+#build containers back up and -d for detach to keep it running in the background
+docker compose -f docker-compose.prod.yml up -d --build
